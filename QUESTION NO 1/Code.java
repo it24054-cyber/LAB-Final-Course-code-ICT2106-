1bc3ba469a0365fd41780c9b5b32ebc67ccac8ce
it24054-cyber <it24054@mbstu.ac.bd>
2026-01-28 15:03:46 +0600

    Create Code.java

---
+++ QUESTION NO 1/Code.java
@@ -0,0 +1,44 @@
+class Student {
+    private int id;
+    private String name;
+    private double cgpa;
+
+    public Student(int id, String name, double cgpa) {
+        this.id = id;
+        this.name = name;
+        setCgpa(cgpa);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(double cgpa) {
+        if (cgpa >= 0.0 && cgpa <= 4.0) {
+            this.cgpa = cgpa;
+        } else {
+            System.out.println("Invalid CGPA! Must be between 0.0 and 4.0");
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student(101, "Afsin", 3.8);
+
+        System.out.println("ID: " + s1.getId());
+        System.out.println("Name: " + s1.getName());
+        System.out.println("CGPA: " + s1.getCgpa());
+
+        s1.setCgpa(4.5); 
+    }
+}
